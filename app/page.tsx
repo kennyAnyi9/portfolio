2d769613c5bfda344aa33e779c9fd8dd887bc8dc
kennyAnyi9 <kennyanyi@gmail.com>
2024-06-13 07:30:44 +0000

    hurray

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 max-w-4xl mx-auto">
+      <h1 className="font-bold text-8xl">Cooking Stuff just be nice</h1>
+    </main>
+  );
+}
